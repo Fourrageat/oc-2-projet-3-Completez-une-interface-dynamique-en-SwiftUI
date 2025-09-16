196a4b983bcd99ce5724779c5f14a347b9dcf29f
Fourrageat <baptiste.fourageat@icloud.com>
2025-09-16 06:51:01 +0200

    wip welcomeView

--- TajMahal/TajMahal/Views/WelcomeView.swift
+++ TajMahal/TajMahal/Views/WelcomeView.swift
@@ -11,11 +11,38 @@ import SwiftUI
 struct WelcomeView: View {
     var body: some View {
         NavigationStack {
-            Image("TajMahal")
+
+            VStack {
+                Image("TajMahal")
+            }
+            
+            HStack {
+                VStack(alignment: .leading, spacing: 8) {
+                    
+                    Text("Restaurant indient")
+                        .font(.custom("Plus jakarta Sans", size: 12))
+                    
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.horizontal)
+                    Text("Taj Mahal")
+                        .font(.custom("Plus jakarta Sans", size: 18))
+                        .fontWeight(.bold)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.horizontal)
+                    
+                }
+                
+                VStack(alignment: .trailing, spacing: 8) {
+                    Image("Logo")
+                }
+            }
+
+
             Spacer()
+
             NavigationLink {
                 MenuView()
-            } label : {
+            } label: {
                 Text("Menu")
             }
         }
