7db92068f1937326d1a9398f956bfdf36e3b4dec
Fourrageat <baptiste.fourageat@icloud.com>
2025-09-24 03:39:28 +0200

    add rowMenuView

---
+++ TajMahal/TajMahal/Views/MenuDetailsView.swift
@@ -0,0 +1,20 @@
+//
+//  MenuDetailsView.swift
+//  TajMahal
+//
+//  Created by Baptiste FOURRAGEAT on 24/09/2025.
+//
+
+import SwiftUI
+
+struct MenuDetailsView: View {
+    let dish: Dish
+    
+    var body: some View {
+
+    }
+}
+
+#Preview {
+
+}
--- TajMahal/TajMahal/Views/MenuView.swift
+++ TajMahal/TajMahal/Views/MenuView.swift
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-var backgroundColorView: Color {
-    Color(red: 241/255, green: 241/255, blue: 241/255)
-}
-
 struct MenuView: View {
     let apetizerArray = ViewModel().apetizerArray
+    let mainCourseArray = ViewModel().mainCourseArray
+    var backgroundColorView: Color {
+        Color(red: 241/255, green: 241/255, blue: 241/255)
+    }
     
     var body: some View {
         
@@ -20,55 +20,66 @@ struct MenuView: View {
             backgroundColorView.ignoresSafeArea()
             
             ScrollView {
-                VStack {
-                    
-                    if apetizerArray.isEmpty {
-                        Text("Aucun plat principal trouvé.")
-                    } else {
-                        ForEach(apetizerArray, id: \.name) { dish in
-                                
-                            HStack(spacing: 12) {
-                                Image(dish.imageName)
-                                    .resizable()
-                                    .frame(width: 112, height: 86)
-                                    .cornerRadius(8)
-                                    .padding(.horizontal, 11)
-                                    .padding(.vertical, 12)
-                                VStack(alignment: .leading) {
-                                    Text(dish.name)
-                                        .font(.system(size: 14))
-                                        .fontWeight(.bold)
-                                        Spacer()
-                                    Text(dish.description)
-                                        .font(.system(size: 12))
-                                        .font(.subheadline)
-                                    Spacer()
-                                    HStack {
-                                        Text("\(dish.price, specifier: "%.2f") €")
-                                            .font(.caption)
-                                        Spacer()
-                                        switch dish.spiceLevel {
-                                        case .hot:
-                                            Text("3")
-                                        case .medium:
-                                            Text("2")
-                                        case .light:
-                                            Text("1")
-                                        }
-                                    }
-                                }
-                                .padding(.vertical, 12)
-                                .padding(.trailing, 16)
-                            }
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .background(Color.white)
-                            .cornerRadius(10)
-                            .padding(.horizontal, 20)
-                        }
-                        
-                    }
-                    
+                VStack(alignment: .leading) {
+                    Text("Entrées")
+                    RowMenuView(menuItems: apetizerArray)
+                    Text("Plats Principaux")
+                        .padding(.top, 12)
+                    RowMenuView(menuItems: mainCourseArray)
                 }
+                .padding(.horizontal, 20)
+            }
+        }
+    }
+}
+
+
+struct RowMenuView: View {
+    let menuItems: [Dish]
+    
+    var body: some View {
+        
+        if menuItems.isEmpty {
+            Text("Aucun plat principal trouvé.")
+        } else {
+            ForEach(menuItems, id: \.name) { dish in
+                    
+                HStack(spacing: 12) {
+                    Image(dish.imageName)
+                        .resizable()
+                        .frame(width: 112, height: 86)
+                        .cornerRadius(8)
+                        .padding(.horizontal, 11)
+                        .padding(.vertical, 12)
+                    VStack(alignment: .leading) {
+                        Text(dish.name)
+                            .font(.system(size: 14))
+                            .fontWeight(.bold)
+                            Spacer()
+                        Text(dish.description)
+                            .font(.system(size: 12))
+                            .font(.subheadline)
+                        Spacer()
+                        HStack {
+                            Text("\(dish.price, specifier: "%.2f") €")
+                                .font(.caption)
+                            Spacer()
+                            switch dish.spiceLevel {
+                            case .hot:
+                                Text("3")
+                            case .medium:
+                                Text("2")
+                            case .light:
+                                Text("1")
+                            }
+                        }
+                    }
+                    .padding(.vertical, 12)
+                    .padding(.trailing, 16)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .background(Color.white)
+                .cornerRadius(10)
             }
         }
     }
