6028b620ff525ad980ec2b2c62518e5f84d9adfc
Fourrageat <baptiste.fourageat@icloud.com>
2025-10-25 20:17:01 +0200

    refactor welcome view

--- TajMahal/TajMahal/Views/WelcomeView.swift
+++ TajMahal/TajMahal/Views/WelcomeView.swift
@@ -6,75 +6,69 @@ import SwiftUI
 struct WelcomeView: View {
     var body: some View {
         NavigationStack {
-            VStack {
-                VStack(alignment: .leading, spacing: 0) {
-                    Image("TajMahal")
-                        .resizable()
-                        .cornerRadius(10)
-
-                    HStack() {
-                        VStack(alignment: .leading, spacing: 0) {
-                            Text("Restaurant Indien")
-                                .foregroundStyle(Color.appGray)
-                                .font(Font.DefaultFont(size: 12))
-
-                            Text("Taj Mahal")
-                                .foregroundStyle(Color.appBlack)
-                                .font(Font.DefaultFont(size: 18))
-                                .fontWeight(.bold)
-                                .padding(.top, 2)
-                        }
-                        Spacer()
-                        Image("GrayLogo")
-                            .frame(width: 40, height: 40)
-                    }
-                    .padding(.top, 32)
-                    VStack(spacing: 14) {
-                        KeyValueRow(
-                            icon: "Horaire",
-                            title: "Mardi",
-                            value: "11h30 – 14h30 • 18h30 – 22h00"
-                        )
-                        KeyValueRow(
-                            icon: "Marmite",
-                            title: "Type de Service",
-                            value: "À emporter"
-                        )
-                        InfoRow(
-                            icon: "Localisation",
-                            text: "12 Avenue de la Brique – 75010 Paris"
-                        )
-                        InfoRow(
-                            icon: "Site",
-                            text: "www.tajmahal.fr",
-                        )
-                        InfoRow(
-                            icon: "Téléphone",
-                            text: "06 12 34 56 78",
-                        )
-                    }
-                    .padding(.top, 32)
-                }
-                .padding(.horizontal, 20)
-                .padding(.vertical, 16)
-                
-            }
-            // Bouton de navigation vers le menu du restaurant
-            NavigationLink {
-                MenuView()
-            } label: {
-                Text("Accéder au menu")
-                    .font(Font.DefaultFont(size: 16))
-                    .fontWeight(.bold)
-                    .font(.headline)
-                    .foregroundStyle(.white)
-                    .frame(maxWidth: .infinity)
-                    .frame(height: 40)
-                    .background(Color.appRed)
+            VStack(alignment: .leading, spacing: 0) {
+                Image("TajMahal")
+                    .resizable()
                     .cornerRadius(10)
+
+                HStack() {
+                    VStack(alignment: .leading, spacing: 0) {
+                        Text("Restaurant Indien")
+                            .foregroundStyle(Color.appGray)
+                            .font(Font.DefaultFont(size: 12))
+
+                        Text("Taj Mahal")
+                            .foregroundStyle(Color.appBlack)
+                            .font(Font.DefaultFont(size: 18))
+                            .fontWeight(.bold)
+                            .padding(.top, 2)
+                    }
+                    Spacer()
+                    Image("GrayLogo")
+                        .frame(width: 40, height: 40)
+                }
+                .padding(.top, 32)
+                VStack(spacing: 14) {
+                    KeyValueRow(
+                        icon: "Horaire",
+                        title: "Mardi",
+                        value: "11h30 – 14h30 • 18h30 – 22h00"
+                    )
+                    KeyValueRow(
+                        icon: "Marmite",
+                        title: "Type de Service",
+                        value: "À emporter"
+                    )
+                    InfoRow(
+                        icon: "Localisation",
+                        text: "12 Avenue de la Brique – 75010 Paris"
+                    )
+                    InfoRow(
+                        icon: "Site",
+                        text: "www.tajmahal.fr",
+                    )
+                    InfoRow(
+                        icon: "Téléphone",
+                        text: "06 12 34 56 78",
+                    )
+                }
+                .padding(.top, 32)
+                
+                NavigationLink {
+                    MenuView()
+                } label: {
+                    Text("Accéder au menu")
+                        .font(Font.DefaultFont(size: 16))
+                        .fontWeight(.bold)
+                        .foregroundStyle(.white)
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 40)
+                        .background(Color.appRed)
+                        .cornerRadius(10)
+                }
+                .padding(.top, 32)
             }
             .padding(.horizontal, 20)
-            .padding(.top, 18)
         }
     }
 }
